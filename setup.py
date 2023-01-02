17b4f4a2575610226ca6950a9330cdc4661f30af
Jonathan <jonathanbesomi@gmail.com>
2023-01-02 17:24:38 +0100

    submodule

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 import setuptools
 
+with open("requirements.txt") as f:
+    required = f.read().splitlines()
+
 if __name__ == "__main__":
     setuptools.setup(
         package_data={"grandmaster": ["py.typed"]},
+        install_requires=required,
     )
